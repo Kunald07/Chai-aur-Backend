c57fe3b8444af6a8b23228d45210b0fc06fb9c93
Kunal Deshmukh <deshmukh1207@gmail.com>
2026-03-03 13:07:46 +0530

    utils code part

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+//using Promise 
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+//using try & catch 
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
